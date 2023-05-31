fdb2cb06d49fde0481186a16f867b4692bff3a25
unknown <luozheyuan@xunlei.com>
2023-05-31 20:02:22 +0800

    feat: 分页

--- model/handleSess.go
+++ model/handleSess.go
@@ -1,9 +1,13 @@
 package model
 
 import (
+	"fmt"
 	"log"
+	"strings"
 	"sync"
 )
+
+const limitlen int = 30
 
 // 一个线程运行此方法,将结束的会话插入数据库中
 // 调用之前记得wg.Add()
@@ -68,6 +72,52 @@ func GetSessByIP(tablename string, ip uint32) []*Session {
 	allSess := make([]*Session, 0)
 	db.Table(tablename).Where("bip = ?", ip).Find(&allSess)
 	return allSess
+}
+
+// 获得条件查询（ip, appname...）的语句
+func getquerystr(ip uint32, appname string) string {
+	querystr := strings.Builder{}
+	if ip != 0 {
+		querystr.WriteString(fmt.Sprintf("ip = '%d'", ip))
+	}
+	if appname != "" {
+		if querystr.Len() != 0 {
+			querystr.WriteString(" AND ")
+		}
+		querystr.WriteString(fmt.Sprintf("appname = '%s'", appname))
+	}
+
+	// if !tt_start.IsZero() && !tt_end.IsZero() {
+	// 	if querystr.Len() != 0 {
+	// 		querystr.WriteString(" AND ")
+	// 	}
+	// 	querystr.WriteString(fmt.Sprintf("test_time BETWEEN '%v' AND '%v'", tt_start, tt_end))
+	// }
+	// if !ct_start.IsZero() && !ct_end.IsZero() {
+	// 	if querystr.Len() != 0 {
+	// 		querystr.WriteString(" AND ")
+	// 	}
+	// 	querystr.WriteString(fmt.Sprintf("create_time BETWEEN '%v' AND '%v'", ct_start, ct_end))
+	// }
+	return querystr.String()
+}
+
+// 条件查询session获得总数量
+func Count_sess_by_term(detectid int, ip uint32, appname string) int {
+	querystr := getquerystr(ip, appname)
+	var num int64
+	if err := db.Table(fmt.Sprintf("detectsess_%d", detectid)).Where(querystr).Count(&num).Error; err != nil {
+		log.Println("db find error:", err)
+	}
+	return int(num)
+}
+
+// 分页条件查询session
+func Find_sess_by_term_page(detectid int, ip uint32, appname string, page int) []Session {
+	querystr := getquerystr(ip, appname)
+	result := make([]Session, 0)
+	db.Table(fmt.Sprintf("detectsess_%d", detectid)).Where(querystr).Limit(limitlen).Offset((page - 1) * limitlen).Find(&result)
+	return result
 }
 
 // 根据给定的时间范围查找会话
